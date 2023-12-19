79d3d9c3f2ef3d8140ee1d9990c4f0ace5e4f502
Bober23 <vanya.popov.2003@gmail.com>
2023-12-19 14:30:08 +0300

    Добавьте файлы проекта.

---
+++ DBKursachApp/Calculator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DBKursachApp
+{
+    public static class Calculator
+    {
+
+        public static int CalculateKdInMPa(string material)
+        {
+            if (material == "Сталь-сталь")
+                return 495;
+            if (material == "Сталь-чугун")
+                return 445;
+            if (material == "Сталь-бронза")
+                return 430;
+            if (material == "Чугун-чугун")
+                return 415;
+            if (material == "Текстолит-сталь")
+                return 200;
+            if (material == "ДСП-сталь")
+                return 225;
+            if (material == "Полиамид-сталь")
+                return 155;  
+            return -1;
+        }
+    }
+}
