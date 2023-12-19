5a862e3f73e607c2e53ef2d019151924a374c72c
Bober23 <vanya.popov.2003@gmail.com>
2023-12-19 16:32:32 +0300

    ready

--- DBKursachApp/Calculator.cs
+++ DBKursachApp/Calculator.cs
@@ -8,22 +8,87 @@ namespace DBKursachApp
 {
     public static class Calculator
     {
-
-        public static int CalculateKdInMPa(string material)
+        public static double CalculateD(int t, int n, string material, string type, double u, double fi, double T1)
         {
-            if (material == "Сталь-сталь")
+            int Kd = CalculateKdInMPa(type);
+            double sigma = CalculateSigmaNPInMPa(t,n,material);
+            return Kd * Math.Pow(T1*1/(u*fi*Math.Pow(sigma,2)), 0.33333333333);
+        }
+        public static double CalculateSigmaNPInMPa(int t, int n, string material)
+        {
+            return CalculateSigmaInMPa(material)*CalculateKnl(t, n, material);
+        }
+        public static long CalculateNHE(int t, int n)
+        {
+            return 60* t * n;
+        }
+        public static double CalculateKnl(int t, int n, string material) 
+        {
+            return Math.Pow(CalculateNHo(material)/CalculateNHE(t,n), 0.1666666666);
+        }
+        public static double CalculateNHo(string material)
+        {
+            if (material == "Сталь 45")
+                return 1.5;
+            if (material == "Сталь 50Г")
+                return 6;
+            if (material == "Сталь 40Х")
+                return 2.5;
+            if (material == "Сталь 40ХН")
+                return 10;
+            if (material == "Сталь 20Х")
+                return 12;
+            if (material == "Сталь 30ХГТ")
+                return 12;
+            if (material == "Чугун ВЧ")
+                return 1;
+            if (material == "Текстолит")
+                return 0.06;
+            if (material == "ДСП")
+                return 0.06;
+            if (material == "Полиамид")
+                return 0.06;
+            return -1;
+        }
+        public static int CalculateSigmaInMPa(string material)
+        {
+            if (material == "Сталь 45")
+                return 600;
+            if (material == "Сталь 50Г")
+                return 800;
+            if (material == "Сталь 40Х")
+                return 650;
+            if (material == "Сталь 40ХН")
+                return 1000;
+            if (material == "Сталь 20Х")
+                return 1100;
+            if (material == "Сталь 30ХГТ")
+                return 1100;
+            if (material == "Чугун ВЧ")
+                return 600;
+            if (material == "Текстолит")
+                return 50;
+            if (material == "ДСП")
+                return 50;
+            if (material == "Полиамид")
+                return 40;
+            return -1;
+        }
+        public static int CalculateKdInMPa(string type)
+        {
+            if (type == "Сталь-сталь")
                 return 495;
-            if (material == "Сталь-чугун")
+            if (type == "Сталь-чугун")
                 return 445;
-            if (material == "Сталь-бронза")
+            if (type == "Сталь-бронза")
                 return 430;
-            if (material == "Чугун-чугун")
+            if (type == "Чугун-чугун")
                 return 415;
-            if (material == "Текстолит-сталь")
+            if (type == "Текстолит-сталь")
                 return 200;
-            if (material == "ДСП-сталь")
+            if (type == "ДСП-сталь")
                 return 225;
-            if (material == "Полиамид-сталь")
+            if (type == "Полиамид-сталь")
                 return 155;  
             return -1;
         }
--- DBKursachApp/MainWindow.xaml.cs
+++ DBKursachApp/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -24,10 +25,52 @@ namespace DBKursachApp
         {
             InitializeComponent();
         }
-
+        private void NumberValidationTextBox(object sender, TextCompositionEventArgs e)
+        {
+            Regex regex = new Regex("[^0-9,]+"); 
+            e.Handled = regex.IsMatch(e.Text);
+        }
         private void calculateButton_Click(object sender, RoutedEventArgs e)
         {
-            
+            int tch = 0;
+            int n = 0;
+            double u= 0;
+            double fi = 0;
+            double T1 = 0;
+            try
+            {
+                if (tchBox.Text != null)
+                {
+                    tch = Convert.ToInt32(tchBox.Text);
+                }
+                if (nBox.Text != null)
+                {
+                    n = Convert.ToInt32(nBox.Text);
+                }
+                string material = materialBox.Text;
+                string type = typeBox.Text;
+                if (uBox.Text != null)
+                {
+                    u = Convert.ToDouble(uBox.Text);
+                }
+                if (fiBox.Text != null)
+                {
+                    fi = Convert.ToDouble(fiBox.Text);
+                }
+                if (t1Box != null)
+                {
+                    T1 = Convert.ToDouble(t1Box.Text);
+                }
+                if (material != null && type != null)
+                {
+                    ansD.Content = Calculator.CalculateD(tch, n, material, type, u, fi, T1);
+                }
+                else ansD.Content = "Введите все данные";
+            }
+            catch
+            {
+                ansD.Content = "Введите все данные корректно";
+            }
         }
     }
 }
